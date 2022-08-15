69224618baa1c7b31ff022b64ce2d030686c9f72
steven12138 <18611209860@163.com>
2022-08-16 01:26:05 +0800

    footer

--- components/FooterBar.js
+++ components/FooterBar.js
@@ -1,15 +1,109 @@
 import styles from "../styles/footer.module.css";
 import {Layout} from "antd";
 import {Image} from "antd";
+import {GithubOutlined} from "@ant-design/icons";
+import {MailOutlined} from "@ant-design/icons";
+import {createFromIconfontCN} from '@ant-design/icons';
 
 const {Footer} = Layout;
+
+const IconFont = createFromIconfontCN({
+    scriptUrl: [
+        "//at.alicdn.com/t/c/font_3590515_e706mgx59.js",
+    ],
+});
+
+const action = [
+        {
+            title: "Github",
+            icon: <GithubOutlined/>,
+            href: "https://github.com/subitlab",
+        },
+        {
+            title: "Email",
+            icon: <MailOutlined/>,
+            href: "mailto:subit@i.pkuschool.edu.cn",
+        },
+        {
+            title: "Yammer",
+            icon: <IconFont type="icon-yammer"/>,
+            href: "https://www.yammer.com/i.pkuschool.edu.cn/#/users/1691511088",
+        },
+        {
+            title: "Telegram",
+            icon: <IconFont type="icon-telegram-plane"/>,
+            href: "https://t.me/+QPaHG_oUu104MjE1",
+        },
+// {
+//     title: "线下联系",
+//     icon: "",
+//     href: null,
+// },
+    ]
+;
 
 export default function FooterBar() {
     return (
         <Footer className={styles.outline}>
             <div className={styles.footer}>
-                <Image alt="logo_revert_transparent" src="/SubIT-Rvt-Transparent.svg"></Image>
+                <div className={styles.link_bar}>
+                    <Image style={{marginTop: "-3px", minWidth: "120px"}} width={120} preview={false}
+                           alt="logo_revert_transparent"
+                           src="/SubIT-Rvt-Transparent.svg"/>
+                    <ActionBar/>
+                </div>
+                <TextLink/>
+                <Copyright/>
             </div>
         </Footer>
+    );
+}
+
+const link = [
+    {title: "关于我们", path: "/about"},
+    {title: "加入我们", path: "/join"},
+    {title: "项目成就", path: "/achievement"},
+    {title: "提供支持", path: "/support"},
+];
+
+function TextLink() {
+    return (
+        <p>
+            {
+                link.map((item, i) => (
+                    <span key={i} style={{color: "white", cursor: "default"}}>
+                        <a href={item.path} className={styles.text_link} style={{
+                            fontSize: "16px",
+                            cursor: "pointer",
+                        }}> {item.title} </a>
+                        &nbsp;{i === link.length - 1 ? "" : "/"}&nbsp;
+                    </span>
+                ))
+            }
+        </p>
+    );
+}
+
+function Copyright() {
+    return (
+        <div className={styles.cpr} style={{color: "white"}}>
+            <span>Copyright © 2018-{new Date().getFullYear()} . All rights reserved. </span>
+            <a className={styles.miit} href="https://beian.miit.gov.cn/">京ICP备2022023326号</a>
+        </div>
+    );
+}
+
+function ActionBar() {
+    return (
+        <div className={styles.action_bar}>
+            {
+                action.map((item, i) => (
+                    <a className={styles.link} key={item.title} href={item.href}>
+                        {item.icon}
+                        <span className={styles.link_text}>{item.title}</span>
+                    </a>
+                ))
+            }
+        </div>
     );
 }
