0d4d897606e28138de0358aff43a942aa9109569
steven12138 <18611209860@163.com>
2022-08-19 14:08:10 +0800

    support css

--- pages/support/index.js
+++ pages/support/index.js
@@ -3,7 +3,7 @@ import {Layout, Image} from "antd";
 import FooterBar from "../../components/FooterBar";
 import std from "../../styles/Home/Home.module.css";
 import PageTitle from "../_title";
-import style from "../../styles/support.module.css";
+import style from "../../styles/Support/support.module.css";
 
 const {Content} = Layout;
 
@@ -30,7 +30,7 @@ export default function Support() {
 
 function Offline() {
     return (
-        <div className={style.parttext}>
+        <div className={style.part_text}>
             <h1 className={style.title}>线下技术支持</h1>
             <p className={style.context}>可用时间</p>
             <p className={style.context}>周一至周五，午休与活动课</p>
@@ -42,13 +42,15 @@ function Offline() {
 
 function Online() {
     return (
-        <div className={style.parttext}>
+        <div className={style.part_text}>
             <h1 className={style.title}>线上技术支持</h1>
             <p className={style.context}>直接发送邮件给SubIT邮箱</p>
             <p className={style.context}>请清晰描述遇到的问题</p>
             <p className={style.context}>后续交流将通过邮件</p>
             <p className={style.context}>如有不便请备注联系方式</p>
-            <a className={style.link} href="mailto:subit@i.pkuschool.edu.cn">发送邮件给SubIT&gt;&gt;</a>
+            <p>
+                <a className={style.link} href="mailto:subit@i.pkuschool.edu.cn">发送邮件给SubIT&gt;&gt;</a>
+            </p>
         </div>
     );
 }
